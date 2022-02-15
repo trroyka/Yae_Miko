ff30e8d3b0ddd9e6d3472f1d7de045f371be5691
trroyka <56067697+trroyka@users.noreply.github.com>
2022-02-15 14:53:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
 const swiper = new Swiper('.main-slider', {
     // Optional parameters
     slidesPerView: 2,
-    spaceBetween: 10,    
+    spaceBetween: 0,    
     loop: true, 
     navigation: {
       nextEl: '.main-slider_arrow',
-    },  
-  });
+    }, 
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768:{
+        slidesPerView:2,
+      }
+    }
+  }); 
+ 
+
+  
+  let burger = document.querySelector('.menu-burger');
+  let closeMenu = document.querySelector('.header__menu-close');
+  let menuMobile = document.querySelector('.header');
+
+  burger.addEventListener('click', ()=> {
+      menuMobile.style.display = 'block';
+  })
+
+  closeMenu.addEventListener('click', ()=>{
+    menuMobile.style.display = 'none';
+  })
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main__button-play');
+
+buttonModal.addEventListener('click', (e)=>{
+  modalWindow.classList.add('active');
+})
+
+modalWindow.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal__inner');
+
+    if(!isModal)
+    {
+          modalWindow.classList.remove('active');
+    }
+})
